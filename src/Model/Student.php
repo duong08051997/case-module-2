2003a1c0fdf262be09119a9ee26ba0f96433cc0d
duong08051997 <duong08051997@gmai.com>
2020-07-20 10:56:45 +0700

    tao lop student

---
+++ src/Model/Student.php
@@ -0,0 +1,119 @@
+<?php
+
+
+namespace App\Model;
+
+
+class Student
+{
+    protected $student_id;
+    protected $name;
+    protected $age;
+    protected $gender;
+    protected $address;
+    protected $email;
+    public function __construct($name,$age,$gender,$address,$email)
+    {
+        $this->name=$name;
+        $this->age=$age;
+        $this->gender=$gender;
+        $this->address=$address;
+        $this->email=$email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStudentId()
+    {
+        return $this->student_id;
+    }
+
+    /**
+     * @param mixed $student_id
+     */
+    public function setStudentId($student_id)
+    {
+        $this->student_id = $student_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
